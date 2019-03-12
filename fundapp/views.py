45e8ada6752a338dc2d8a627f99b19257185de65
XiangLi <xiangli.lu@outlook.com>
2019-03-13 07:54:35 +0800

    RESTful API complete

--- fundapp/views.py
+++ fundapp/views.py
@@ -6,56 +6,49 @@ from django.shortcuts import redirect, render
 from fundapp.profit_test import img
 from datetime import datetime
 from sqlalchemy import create_engine
-from collections import OrderedDict, defaultdict
+from collections import defaultdict
 
-response_data = {}
 
 def test(request):
     if request.method == "POST":
-        global response_data
-        # profit_img, mds_img, profit_indicator, mean_similarity = img(start=datetime.strptime("-".join([request.POST['start_year'], request.POST['start_month']]), '%Y-%m'),
-        #                                                              end=datetime.strptime(
-        #                                                                  "-".join([request.POST['end_year'], request.POST['end_month']]), '%Y-%m'),
-        #                                                              investement_type=np.asarray(
-        #                                                                  request.POST['investement_type'].split(" ")),
-        #                                                              sharpe_ratio=request.POST['sharpe_ratio'],
-        #                                                              std=request.POST['std'],
-        #                                                              beta=request.POST['beta'],
-        #                                                              treynor_ratio=request.POST['treynor_ratio'],
-        #                                                              btest_time=int(
-        #                                                                  request.POST['btest_time']),
-        #                                                              money=int(
-        #                                                                  request.POST['money']),
-        #                                                              buy_ratio=np.asarray(
-        #     [float(request.POST['buy_ratio' + str(i)]) for i in range(4)], dtype=np.float),
-        #     strategy=int(request.POST['strategy']),
-        #     frequency=int(request.POST['frequency'])
-        # )
-
-        response_data = img(start=datetime.strptime("-".join([request.POST['start_year'], request.POST['start_month']]), '%Y-%m'),
-                            end=datetime.strptime("-".join([request.POST['end_year'], request.POST['end_month']]), '%Y-%m'),
-                            investement_type=np.asarray(request.POST['investement_type'].split(" ")),
-                            sharpe_ratio=request.POST['sharpe_ratio'],
-                            std=request.POST['std'],
-                            beta=request.POST['beta'],
-                            treynor_ratio=request.POST['treynor_ratio'],
-                            btest_time=int(request.POST['btest_time']),
-                            money=int(request.POST['money']),
-                            buy_ratio=np.asarray([float(request.POST['buy_ratio' + str(i)]) for i in range(4)], dtype=np.float),
-                            strategy=int(request.POST['strategy']),
-                            frequency=int(request.POST['frequency'])
-        )
-
-        # return render(request, "test_show.html", {'profit_img': profit_img,
-        #                                           'mds_img': mds_img["-".join([request.POST['start_year'], request.POST['start_month']])],
-        #                                           'profit_indicator': profit_indicator,
-        #                                           'mean_similarity': mean_similarity})
-        return render(request, "test_show.html",{'response_data':response_data})
+        url = "/test/start_year=" + request.POST['start_year'] + \
+            " start_month=" + request.POST['start_month'] + \
+            " end_year=" + request.POST['end_year'] + \
+            " end_month=" + request.POST['end_month'] + \
+            " investement_type=" + request.POST['investement_type'] + \
+            " sharpe_ratio=" + request.POST['sharpe_ratio'] + \
+            " std=" + request.POST['std'] + \
+            " beta=" + request.POST['beta'] + \
+            " treynor_ratio=" + request.POST['treynor_ratio'] + \
+            " btest_time=" + request.POST['btest_time'] + \
+            " money=" + request.POST['money'] + \
+            " buy_ratio0=" + request.POST['buy_ratio0'] + \
+            " buy_ratio1=" + request.POST['buy_ratio1'] + \
+            " buy_ratio2=" + request.POST['buy_ratio2'] + \
+            " buy_ratio3=" + request.POST['buy_ratio3'] + \
+            " strategy=" + request.POST['strategy'] + \
+            " frequency=" + request.POST['frequency'] + "/"
+        return redirect(url)
     return render(request, "test.html", locals())
 
 
-def mds(request):
-    global response_data
+def test_show(request, start_year, start_month, end_year, end_month, investement_type, sharpe_ratio, std, beta, treynor_ratio, btest_time, money, buy_ratio0, buy_ratio1, buy_ratio2, buy_ratio3, strategy, frequency):
+    return render(request, "test_show.html", locals())
+
+
+def test_respoonse(request, start_year, start_month, end_year, end_month, investement_type, sharpe_ratio, std, beta, treynor_ratio, btest_time, money, buy_ratio0, buy_ratio1, buy_ratio2, buy_ratio3, strategy, frequency):
+    response_data = img(start=datetime.strptime("-".join([start_year, start_month]), '%Y-%m'),
+                        end=datetime.strptime("-".join([end_year, end_month]), '%Y-%m'),
+                        investement_type=np.asarray(investement_type.split(" ")),
+                        sharpe_ratio=sharpe_ratio,
+                        std=std,
+                        beta=beta,
+                        treynor_ratio=treynor_ratio,
+                        btest_time=btest_time,
+                        money=money,
+                        buy_ratio=np.asarray([float(buy_ratio0), float(buy_ratio1), float(buy_ratio2), float(buy_ratio3)], dtype=np.float),
+                        strategy=strategy,
+                        frequency=frequency)
     return JsonResponse(response_data)
 
 
--- static/javascript/test_show.js
+++ static/javascript/test_show.js
@@ -1,7 +1,17 @@
 $(function () {
+  
   var response_data;
-  $.getJSON('/mds_img/', function (ret) {
+  $.getJSON(location.pathname + 'response/', function (ret) {
     response_data = ret;
+    $("#sharpe_ratio").text("夏普指標: " + response_data.sharpe_ratio);
+    $("#std").text("標準差: " + response_data.std);
+    $("#beta").text("beta: " + response_data.beta);
+    $("#treynor_ratio").text("Treynor指數: " + response_data.treynor_ratio);
+    $("#money").text("期末金額: " + response_data.money);
+    $("#profit").text("最後收益: " + response_data.profit);
+    $("#mean_similarity").text("平均距離: " + response_data.mean_similarity);
+    $("#profit").html(response_data['profit_img'].div + response_data['profit_img'].script);
+    $("#wait").remove();
   })
 
   var temp = ''
